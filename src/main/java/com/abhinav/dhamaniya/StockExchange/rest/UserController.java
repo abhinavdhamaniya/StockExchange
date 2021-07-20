8a6b5979cd3d19079a0233c644717532a4679fde
abhinavdhamaniya <abhinavdhamaniya@gmail.com>
2021-07-20 18:18:55 +0530

    Added cross origin to controllers

--- src/main/java/com/abhinav/dhamaniya/StockExchange/rest/UserController.java
+++ src/main/java/com/abhinav/dhamaniya/StockExchange/rest/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("users")
 public class UserController {
